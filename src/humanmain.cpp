c83676d596176ad106064b63f25cf8599be48ae2
Mishika <144824349+zhsimyrrehs@users.noreply.github.com>
2024-05-09 23:56:22 +0530

    docs: added comments for better understanding

--- src/humanmain.cpp
+++ src/humanmain.cpp
@@ -34,13 +34,14 @@ int main() {
 
     char choice;
     do {
-        cout << "Welcome to ASE - an Academic Search Engine - to ace your academia " << endl;
-        cout << "This tool allows you to perform the following operations!" << endl;
+        cout << "Choose an option:\n";
         cout << "1. Search notes\n";
         cout << "2. List notes\n";
-        cout << "3. Delete a PRIVATE note\n";
-        cout << "4. Exit\n";
-        cout << "Enter your preferred choice (1-4): " << endl;
+	    cout << "3. Create a PRIVATE note\n";
+	    cout << "4. Modify a PRIVATE note\n";
+        cout << "5. Delete a PRIVATE note\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice (1-6): ";
         cin >> choice;
 
         // the user input is stored in choice
@@ -53,7 +54,7 @@ int main() {
 
         // getline takes input (cin) and puts it in query, also no input buffer of \n so we don't have to use cin.ignore() after
         // <string> header
-        // 
+        // searchNotes function- private_notes vector and query string
 
         switch (choice) {
             case '1': {
@@ -64,16 +65,57 @@ int main() {
                 searchNotes(private_notes, query);
                 break;
             }
+
+            // listing
+            // for loop iterates over each note in public and private_notes vector and displays it
+
             case '2': {
                 cout << "Listing notes sorted by time of creation:\n";
                 // TODO: Create function to sort notes by creation time -- use GPT
                 // sortNotesByCreationTime(private_notes);
+                cout << "Public Notes" << endl;
+                for (const auto& note : public_notes) note ->display();
+                cout << "Private Notes" << endl;
                 for (const auto& note : private_notes) note->display();
-
                 // TODO: Also list public notes, after sorting
                 break;
             }
-            case '3': {
+
+            // this is for creation of new note
+            // we take heading as input and put it in heading - we assign heading to content and make a new note (dynamic allocation)
+            // we save it and note content is saved 
+            // private_note object is pushed back into private_note vector 
+
+
+	        case '3': {
+            string heading, content;
+                cout << "Enter the HEADING of the new note:\n";
+                cin.ignore(); 
+                getline(cin, heading);
+                content = heading;
+            
+                Note* note = new PrivateNote(heading, content);
+                note->save();
+                private_notes.push_back(note);
+                    
+                break;
+            }
+	        case '4': {
+                string heading;
+                cout << "Enter the heading private note to modify: ";
+                cin.ignore(); // To consume the newline character left by cin
+                getline(cin, heading);
+		        for (auto& note : private_notes) {
+                    if (note->getHeading() == heading) {
+                        dynamic_cast<PrivateNote*>(note)->edit(heading);
+                        break;
+                    }
+                }
+                //edit(file);
+                // TODO link the code to txt file to directly open the txt folder.
+                break;
+            }
+            case '5': {
                 string heading;
                 cout << "Enter the heading of the private note to delete: ";
                 cin.ignore(); // To consume the newline character left by cin
@@ -82,13 +124,13 @@ int main() {
                 deleteNote(private_notes, heading, FolderType::PRIVATE);
                 break;
             }
-            case '4':
+            case '6':
                 cout << "Exiting application.\n";
                 break;
             default:
-                cout << "Invalid choice. Please enter a number between 1 and 4.\n";
+                cout << "Invalid choice. Please enter a number between 1 and 5.\n";
         }
-    } while (choice != '4');
+    } while (choice != '6');
 
     for (auto& note : public_notes) delete note;
     for (auto& note : private_notes)delete note;
